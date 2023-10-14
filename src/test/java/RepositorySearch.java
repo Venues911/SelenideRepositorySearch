6286b09a2ee1b07c0c940a14438b3dc48601f8bb
IvP <vanusezzz@gmail.com>
2023-10-14 15:37:23 +0300

    Finish test

--- src/test/java/RepositorySearch.java
+++ src/test/java/RepositorySearch.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
 
 public class RepositorySearch {
     @Test
-    void HaveTextSelenideAtTheTop (){
+    void HaveTextSelenideAtTheTop() {
         // First step to any test - Manual Test that you want automation
         // Write your step - planning what you want to do
         //- Откройте страницу Selenide в Github
@@ -17,7 +19,27 @@ public class RepositorySearch {
         $("[placeholder='Search or jump to...']").click(); // .class, #id, []
         $("#query-builder-test").setValue("Selenide").pressEnter();
         $$("[data-testid='results-list'] div").first().$("a").click();
+        $("#wiki-tab").click();
+        $(".wiki-more-pages-link").$("button").click();
+//        $( ".js-wiki-sidebar-toggle-display")
+        $$("[data-filterable-for='wiki-pages-filter'] li").findBy(text("SoftAssertions")).$("a").click();
 
+        $$(".markdown-body h4").filterBy(text("Using JUnit5 extend test class:")).shouldHave(size(1));
+        $$(".markdown-body h4").filterBy(text("Using JUnit5 extend test class:")).
+                first().sibling(0).$("pre").shouldHave(text(
+                        """
+                                   @ExtendWith({SoftAssertsExtension.class})
+                                                                     class Tests {
+                                                                       @Test
+                                                                       void test() {
+                                                                         Configuration.assertionMode = SOFT;
+                                                                         open("page.html");
+     
+                                                                         $("#first").should(visible).click();
+                                                                         $("#second").should(visible).click();
+                                       }                       
+                                     }
+                        """
+                ));
     }
-
 }
